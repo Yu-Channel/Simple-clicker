22bacc8e7c31b7351b5f4625ff5d395c9ef43d19
Yu-Channel <hagenosenshi@gmail.com>
2024-03-31 01:43:30 +0900

    Mainのスクリプトを作成

---
+++ scripts/Main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# グローバル変数
+## プレイヤーのステータス
+var click_power				# クリック攻撃力
+var click_multiplier		# クリック倍率 *実装は未定
+var auto_click_power		# オートクリック攻撃力
+var auto_click_multiplier	# オートクリック倍率
+var auto_click_cooldown		# オートクリック速度
+
+
+func _ready():
+	pass # Replace with function body.
+
