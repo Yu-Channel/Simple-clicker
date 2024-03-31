b8423f150effd9591186e6379883f3766f5f7666
Yu-Channel <hagenosenshi@gmail.com>
2024-04-01 03:12:27 +0900

    MainノードにEnemyのインスタンスを作成を追加

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+var enemy_node
 
 func _ready():
-	pass
+	enemy_node = load("res://scene/Enemy.tscn").instance()
+	add_child(enemy_node)
+	enemy_node.position.x = 260
+	enemy_node.position.y = 140
+
+
 
