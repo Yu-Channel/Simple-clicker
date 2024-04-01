441b1cb64c9019f10f7601159894f0a2942516cd
Yu-Channel <hagenosenshi@gmail.com>
2024-04-02 05:01:56 +0900

    敵を倒した数に応じて階層を変えて、HPが変化するように追加

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 
 func _ready():
-	Grobal.enemy_max_hp = 10
-	Grobal.enemy_hp = Grobal.enemy_max_hp
+	if Grobal.floor_num < 1:
+		Grobal.floor_num = 1
+	
+	set_enemy_hp()
+
+func _process(delta):
+	pass
 	
 
 func _on_Enemy_input_event(viewport, event, shape_idx):
@@ -13,7 +18,18 @@ func _on_Enemy_input_event(viewport, event, shape_idx):
 			print(str(Grobal.enemy_hp)) # チェック用
 			
 			if Grobal.enemy_hp <= 0:
-				
 				queue_free()
 			
+func set_enemy_hp():
+	var enemy_hp:float = 0
+	enemy_hp = int(10 * (1 + Grobal.floor_num * 0.2))
+	
+	# 10の倍数ごとにボス
+	if !(Grobal.floor_num % 10):
+		enemy_hp *= 1 + (pow(Grobal.floor_num / 10, 2) * 0.1)
+	
+	Grobal.enemy_max_hp = int(enemy_hp)	
+	# 現在HPに最大HPと同じ値を代入
+	Grobal.enemy_hp = Grobal.enemy_max_hp
+
 
