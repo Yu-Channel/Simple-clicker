0e8943889a449ac858113a6afb6d09be846ce699
Yu-Channel <hagenosenshi@gmail.com>
2024-04-02 05:00:05 +0900

    デバッグ表示のため変更

--- scripts/Test_label.gd
+++ scripts/Test_label.gd
@@ -3,5 +3,5 @@ extends Label
 onready var enemy_cooldown = $"../EnemyCooldown"
 
 func _process(delta):
-	text = str(Grobal.enemy_hp) + "/Timer= " + str(enemy_cooldown.wait_time)
+	text = "HP:" + str(Grobal.enemy_hp) + " floor:" + str(Grobal.floor_num)
 
