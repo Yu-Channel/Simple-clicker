e83a8e53600271210c4b9c11322bec0b468c3f63
Yu-Channel <hagenosenshi@gmail.com>
2024-04-02 02:42:59 +0900

    Enemyシーンをインスタンス化して呼び出したときのシグナル受信の処理を追加

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,10 +3,15 @@ extends Node2D
 var enemy_node
 
 func _ready():
+	call_enemy()
+
+func call_enemy():
 	enemy_node = load("res://scene/Enemy.tscn").instance()
+	enemy_node.connect("tree_exited", self, "_on_Enemy_tree_exited")
 	add_child(enemy_node)
 	enemy_node.position.x = 260
 	enemy_node.position.y = 140
-
-
+	
+func _on_Enemy_tree_exited():
+	print("子ノードが消えたよ")
 
